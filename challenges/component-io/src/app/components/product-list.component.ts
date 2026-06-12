86c93ce2b191a52d6304fd999d295aa1e35dfe02
Paul Buergin <paul.buergin@hftm.ch>
2026-06-12 17:58:28 +0200

    fix: 4 Fehler korrigiert

--- challenges/component-io/src/app/components/product-list.component.ts
+++ challenges/component-io/src/app/components/product-list.component.ts
@@ -9,11 +9,11 @@ import { ProductCardComponent } from './product-card.component';
   template: `
     <h2>Produktkatalog</h2>
 
-    @if (products().length === 0) {
+    @if (products().length > 0) {
       <div class="product-grid">
-        @for (product of products(); track product.titel) {
+        @for (product of products(); track product.id) {
           <app-product-card
-            [produkt]="product"
+            [product]="product"
             (onLiked)="handleLike($event)"
           />
         }
